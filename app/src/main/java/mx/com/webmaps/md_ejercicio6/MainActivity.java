89f802d9efcdee4e6609aef21e300ec3fa48026a
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-16 16:46:28 -0500

    Se crea un AlertDialog con sus botones de aceptar y cancelar

--- app/src/main/java/mx/com/webmaps/md_ejercicio6/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio6/MainActivity.java
@@ -1,13 +1,44 @@
 package mx.com.webmaps.md_ejercicio6;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlertDialog alertDialog;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setMessage("Discard Draft");
+
+        builder.setPositiveButton("DISCARD", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("Discard");
+
+            }
+        });
+
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.out.println("cancel");
+            }
+        });
+
+        alertDialog = builder.create();
+
+        alertDialog.show();
+
+
     }
 }
